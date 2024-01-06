6d4d8750e15c3f632742d81362ecba081661d557
sunny <nepalsanjog@gmail.com>
2024-01-06 08:47:23 -0600

    deposite updated

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    # Your main code goes here
-    print("Hello, world!")
 
-if __name__ == "__main__":
-    main()
+def deposit():
+    while True:
+        amount = input("what would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print ("please enter a number.")
+    return amount
+
+    
+
+def main():
+    balance = deposit()
